36e7b8bae86be1230e45fa0c8a246625b0699f17
Kai Storbeck <kai@xs4all.nl>
2023-12-15 22:47:05 +0100

    day12 typing and some caching

--- day12-arrangements/day12.py
+++ day12-arrangements/day12.py
@@ -1,40 +1,47 @@
 
 from baseday import BaseDay
 import itertools
+from collections.abc import Iterator
+from typing import Tuple
 
 class Day12(BaseDay):
     rows: list[str]
 
-    def init(self):
+    def init(self) -> None:
         with open(self.input) as fh:
             self.rows=[ x.strip() for x in fh.readlines() ]
 
-    def part1(self):
+    def part1(self) -> None:
         # loop over all possibilities of ?, and see if 
         # that could fit.
 
-        def are_broken(s: str):
+        def are_broken(s: str) -> Tuple[int, ...]:
             """ .#...#....###. -> (1,1,3)"""
             return tuple(len(x) for x in s.split('.') if '#' in x)
 
-        def options(s: str, negcache: set):
-            for x in list(itertools.product(['.', '#'], repeat=s.count('?'))):
+        def options(s: str, totalcount: int, negcache: set) -> Iterator[str]:
+            for x in itertools.product(['.', '#'], repeat=s.count('?')):
                 t = s.replace('?', '{}').format(*x)
+                if t.count('#') != totalcount: continue
                 if t not in negcache:
                     yield t
 
-        def split(s: str):
+        def split(s: str) -> Tuple[str, Tuple[int, ...]]:
             s, errors = s.split(' ')
             return (s, tuple(int(x) for x in errors.split(',')))
 
-        def count_arrangements(springs) -> int:
+        def count_arrangements(springs: str) -> int:
             s, errors = split(springs)
-            #print(s, errors)
+            if self.example:
+                print(s, errors)
             arrangements = 0
-            ncache = set()
-            for o in options(s, ncache):
-                if are_broken(o) == errors:
+            ncache: set[str] = set()
+            pcache: set[str] = set()
+            for o in options(s, sum(errors), ncache):
+                if o in ncache: continue
+                if o in pcache or are_broken(o) == errors:
                     arrangements+=1
+                    pcache.add(o)
                 else:
                     ncache.add(o)
             return arrangements
