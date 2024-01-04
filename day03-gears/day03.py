170a33c73de734f23ac23a4ad6c57dc80778d023
Kai Storbeck <kai@xs4all.nl>
2024-01-04 23:24:41 +0100

    added faster version based on others ideas

--- day03-gears/day03.py
+++ day03-gears/day03.py
@@ -20,13 +20,13 @@ nonsymbols = '0123456789.'
 numfind = re.compile(r'([0-9]+)')
 
 class Day03(BaseDay):
-    def init(self):
+    def init(self) -> None:
         with open(self.input) as fh:
             self.data = [line.strip() for line in fh.readlines()]
             self.h = len(self.data)
             self.w = len(self.data[0])
 
-    def part1(self):
+    def part1(self) -> None:
         # build a map with numbers, and
         # a map with points that refer to the key of the numbers
         symbols: set[Point] = set()
@@ -45,13 +45,12 @@ class Day03(BaseDay):
         uniq = set()
         for point in symbols:
             for dx, dy in wind: # adjacency
-                t = Point(point.x+dx, point.y+dy)
                 if (point.x+dx, point.y+dy) in p2n:
-                    uniq.add(p2n[(point.x+dx, point.y+dy)])
+                    uniq.add(p2n[Point(point.x+dx, point.y+dy)])
 
         print(sum([numbers[x] for x in uniq]))
 
-    def part2(self):
+    def part2(self) -> None:
         symbols: set[Point] = set()
         numbers: dict[Point, int] = {}
         p2n: dict[Point, Point] = {}
@@ -61,16 +60,35 @@ class Day03(BaseDay):
                 for ny in range(span[0], span[1]):
                     p2n[Point(x, ny)] = here
                 numbers[here] = int(line[span[0]:span[1]])
-            [ symbols.add(Point(x, y)) for y in range(len(line)) if line[y] == '*' ]
+            map(symbols.add, [Point(x, y) for y in range(len(line)) if line[y] == '*' ])
 
         total = 0
         for point in symbols:
             two = set()
             for dx, dy in wind: # adjacency
-                t = Point(point.x+dx, point.y+dy)
                 if (point.x+dx, point.y+dy) in p2n:
-                    two.add(p2n[(point.x+dx, point.y+dy)])
+                    two.add(p2n[Point(point.x+dx, point.y+dy)])
             if len(two) == 2:
                 total += math.prod([numbers[x] for x in two])
 
+        print(total)
+
+    # topaz.github.io: this never maps any numbers that have no overlap
+    def part3(self) -> None:
+        total = 0
+        for x in range( self.h ):
+            for y in range( self.w ):
+                if self.data[ x ][ y ] != '*': continue
+
+                parts = []
+                for j in range( max( x - 1, 0 ), min( x + 2, self.h ) ):
+                    start, end = y, y
+                    while start > 0 and self.data[ j ][ start - 1 ].isdigit():
+                        start -= 1
+                    while end < self.w - 1 and self.data[ j ][ end + 1 ].isdigit():
+                        end += 1
+                    for num in [int(x) for x in numfind.findall(self.data[j][start:end+1])]:
+                        parts.append(num)
+                if len( parts ) == 2:
+                    total += math.prod(parts)
         print(total)
