3d8671b59aab82a5fbd6293ced51f6f048f452a8
Kai Storbeck <kai@xs4all.nl>
2024-01-05 15:31:48 +0100

    part 2, black, mypy

--- day13-reflections/day13.py
+++ day13-reflections/day13.py
@@ -1,42 +1,56 @@
 from baseday import BaseDay
 from itertools import pairwise
 
-class Block():
+
+def cmp(ar1: str, ar2: str) -> int:
+    return sum([x != y for x, y in zip(ar1, ar2)])
+
+
+class Block:
     w = 0
     h = 0
-    content = ''
+    content = ""
 
     def __init__(self, w: int, h: int, content: str) -> None:
         self.w = w
         self.h = h
         self.content = content
 
-    def row(self, i: int) -> str: # row(1) == index[0]
-        offset = (i-1) * self.w
-        return self.content[offset:offset+self.w]
+    def row(self, i: int) -> str:  # row(1) == index[0]
+        offset = (i - 1) * self.w
+        return self.content[offset : offset + self.w]
 
     def col(self, i: int) -> str:
-        offset = i-1
-        return self.content[offset::self.w]
+        offset = i - 1
+        return self.content[offset :: self.w]
 
-    def scan_h(self) -> int:
-        for a, b in pairwise(range(1, self.h+1)):
-            if self.row(a) == self.row(b):
-                size = min(a-1, self.h - b)
-                if all(self.row(a-i) == self.row(b+i) for i in range(1, size+1)):
+    def scan_h(self, maxdelta: int = 0) -> int:
+        for a, b in pairwise(range(1, self.h + 1)):
+            delta = cmp(self.row(a), self.row(b))
+            if 0 <= delta <= maxdelta:
+                size = min(a - 1, self.h - b)
+                delta += sum(
+                    cmp(self.row(a - i), self.row(b + i)) for i in range(1, size + 1)
+                )
+                if delta == maxdelta:
                     return a * 100
         return 0
 
-    def scan_v(self) -> int:
-        for a, b in pairwise(range(1, self.w+1)):
-            if self.col(a) == self.col(b):
-                size = min(a-1, self.w - b)
-                if all(self.col(a-i) == self.col(b+i) for i in range(1, size+1)):
+    def scan_v(self, maxdelta: int = 0) -> int:
+        for a, b in pairwise(range(1, self.w + 1)):
+            delta = cmp(self.col(a), self.col(b))
+            if 0 <= delta <= maxdelta:
+                size = min(a - 1, self.w - b)
+                delta += sum(
+                    cmp(self.col(a - i), self.col(b + i)) for i in range(1, size + 1)
+                )
+                if delta == maxdelta:
                     return a
         return 0
 
+
 class Day13(BaseDay):
-    blocks: list[Block]
+    blocks: list[Block] = []
 
     def init(self) -> None:
         with open(self.input) as fh:
@@ -45,23 +59,23 @@ class Day13(BaseDay):
             for line in (x.strip() for x in fh.readlines()):
                 # new block
                 if not line:
-                    self.blocks.append(Block(width, height, ''.join(content)))
+                    self.blocks.append(Block(width, height, "".join(content)))
                     width = height = 0
                     content = []
-                    #print()
+                    # print()
                     continue
                 if self.example:
                     print(line)
 
-                height+=1
+                height += 1
                 w = len(line)
                 if not width:
                     width = w
                 elif width != w:
-                    print(f'weird length on {line}; C={content} w={width} h={height}')
+                    print(f"weird length on {line}; C={content} w={width} h={height}")
                 content.append(line)
 
-            self.blocks.append(Block(width, height, ''.join(content)))
+            self.blocks.append(Block(width, height, "".join(content)))
 
     def part1(self) -> None:
         total = 0
@@ -72,4 +86,8 @@ class Day13(BaseDay):
 
     def part2(self) -> None:
         # TBD
-        pass
+        total = 0
+        for b in self.blocks:
+            total += b.scan_v(1)
+            total += b.scan_h(1)
+        print(total)
