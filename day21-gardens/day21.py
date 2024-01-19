ce55330fca11fd62393010d096383cbd0898de0a
Kai Storbeck <kai@xs4all.nl>
2024-01-19 23:54:12 +0100

    black & mypy

--- day21-gardens/day21.py
+++ day21-gardens/day21.py
@@ -2,40 +2,48 @@ from baseday import BaseDay
 from collections import namedtuple
 from itertools import pairwise
 
-def isgarden(a):
-    return a == '.'
+Point = namedtuple("Point", ["x", "y"])
 
-Point = namedtuple('Point', ['x', 'y'])
 
 class Day21(BaseDay):
-    def init(self):
+    def init(self) -> None:
         with open(self.input) as fh:
             self.map = [[x for x in line.strip()] for line in fh.readlines()]
-        self.h = len(self.map) # x
-        self.w = len(self.map[0]) # y
+        self.h = len(self.map)  # x
+        self.w = len(self.map[0])  # y
+
     def find_s(self) -> Point:
         for y, line in enumerate(self.map):
-            if 'S' in line:
-                return Point(line.index('S'), y)
+            if "S" in line:
+                return Point(line.index("S"), y)
         raise ValueError()
-    def printmap(self, rightnow=set()):
+
+    def printmap(self, rightnow: set = set()) -> None:
         c = [row.copy() for row in self.map]
         for p in rightnow:
-            c[p.x][p.y] = 'O'
+            c[p.x][p.y] = "O"
         for row in c:
-            print(''.join(row))
+            print("".join(row))
 
-    def possibilities(self, p, steps):
+    def possibilities(self, p: Point, steps: int) -> None:
         def move_once(source: set[Point]) -> set[Point]:
             dest = set()
             for p in source:
                 for dx, dy in (
-                        (p.x, p.y-1),
-                        (p.x, p.y+1),
-                        (p.x-1, p.y),
-                        (p.x+1, p.y),
-                        ):
-                    if any((dx < 0, dx > self.h, dy < 0, dy > self.w, self.map[dx][dy] != '.')):
+                    (p.x, p.y - 1),
+                    (p.x, p.y + 1),
+                    (p.x - 1, p.y),
+                    (p.x + 1, p.y),
+                ):
+                    if any(
+                        (
+                            dx < 0,
+                            dx > self.h,
+                            dy < 0,
+                            dy > self.w,
+                            self.map[dx][dy] != ".",
+                        )
+                    ):
                         continue
                     dest.add(Point(dx, dy))
             return dest
@@ -50,15 +58,15 @@ class Day21(BaseDay):
                 self.printmap(reach)
         print(len(reach))
 
-
-    def part1(self):
+    def part1(self) -> None:
         start = self.find_s()
-        self.map[start.x][start.y] = '.' # it is a garden step
+        self.map[start.x][start.y] = "."  # it is a garden step
         if self.example:
             self.printmap()
         steps = 64
-        if self.example: steps = 6
+        if self.example:
+            steps = 6
         self.possibilities(start, steps)
 
-    def part2(self):
+    def part2(self) -> None:
         pass
