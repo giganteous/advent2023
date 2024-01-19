d8597b445fb3e812728d7f0b29e6314e29ac0856
Kai Storbeck <kai@xs4all.nl>
2024-01-19 23:46:08 +0100

    black and mypy

--- day12-arrangements/day12.py
+++ day12-arrangements/day12.py
@@ -1,34 +1,35 @@
-
 from baseday import BaseDay
 import itertools
 from collections.abc import Iterator
 from typing import Tuple
+
 
 class Day12(BaseDay):
     rows: list[str]
 
     def init(self) -> None:
         with open(self.input) as fh:
-            self.rows=[ x.strip() for x in fh.readlines() ]
+            self.rows = [x.strip() for x in fh.readlines()]
 
     def part1(self) -> None:
-        # loop over all possibilities of ?, and see if 
+        # loop over all possibilities of ?, and see if
         # that could fit.
 
         def are_broken(s: str) -> Tuple[int, ...]:
-            """ .#...#....###. -> (1,1,3)"""
-            return tuple(len(x) for x in s.split('.') if '#' in x)
+            """.#...#....###. -> (1,1,3)"""
+            return tuple(len(x) for x in s.split(".") if "#" in x)
 
         def options(s: str, totalcount: int, negcache: set) -> Iterator[str]:
-            for x in itertools.product(['.', '#'], repeat=s.count('?')):
-                t = s.replace('?', '{}').format(*x)
-                if t.count('#') != totalcount: continue
+            for x in itertools.product([".", "#"], repeat=s.count("?")):
+                t = s.replace("?", "{}").format(*x)
+                if t.count("#") != totalcount:
+                    continue
                 if t not in negcache:
                     yield t
 
         def split(s: str) -> Tuple[str, Tuple[int, ...]]:
-            s, errors = s.split(' ')
-            return (s, tuple(int(x) for x in errors.split(',')))
+            s, errors = s.split(" ")
+            return (s, tuple(int(x) for x in errors.split(",")))
 
         def count_arrangements(springs: str) -> int:
             s, errors = split(springs)
@@ -38,9 +39,10 @@ class Day12(BaseDay):
             ncache: set[str] = set()
             pcache: set[str] = set()
             for o in options(s, sum(errors), ncache):
-                if o in ncache: continue
+                if o in ncache:
+                    continue
                 if o in pcache or are_broken(o) == errors:
-                    arrangements+=1
+                    arrangements += 1
                     pcache.add(o)
                 else:
                     ncache.add(o)
@@ -48,6 +50,5 @@ class Day12(BaseDay):
 
         print(sum(count_arrangements(x) for x in self.rows))
 
-    def part2(self) -> None
-        pass # Got stuck on a happy path. TBD
-
+    def part2(self) -> None:
+        pass  # Got stuck on a happy path. TBD
