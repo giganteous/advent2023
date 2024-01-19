8262944c457f567d3276f4f3187bcc8398538f37
Kai Storbeck <kai@xs4all.nl>
2024-01-19 23:29:56 +0100

    black & mypy

--- day08-haunt/day08.py
+++ day08-haunt/day08.py
@@ -1,57 +1,63 @@
 from baseday import BaseDay
 import re
-
 import math
+from typing import Iterable, Any
+
 
 class Day08(BaseDay):
     directions: str
-    maze: dict[tuple[str, str]]
+    maze: dict[str, list[str | Any]] = {}
 
-    def init(self):
+    def init(self) -> None:
         with open(self.input) as fh:
             directions = fh.readline().strip()
             data = fh.readlines()
 
-        maze: dict[tuple[str, str]] = {}
-        m = re.compile(r'^(?P<here>[A-Z0-9]{3}) = \((?P<left>[A-Z0-9]{3}), (?P<right>[A-Z0-9]{3})\)')
+        m = re.compile(
+            r"^(?P<here>[A-Z0-9]{3}) = \((?P<left>[A-Z0-9]{3}), (?P<right>[A-Z0-9]{3})\)"
+        )
         for line in data:
             r = m.match(line.strip())
             if not r:
                 continue
-            maze[r.group('here')] = [r.group('left'), r.group('right')]
-        self.maze = maze
+            self.maze[r.group("here")] = [r.group("left"), r.group("right")]
         self.directions = directions
 
-    def route(self):
+    def route(self) -> Iterable:
         l = len(self.directions)
-        i = [(x == 'R') and 1 or 0 for x in self.directions]
+        i = [(x == "R") and 1 or 0 for x in self.directions]
         x = 0
         while True:
-            yield i[x%l]
-            x+=1
+            yield i[x % l]
+            x += 1
 
-    def part1(self):
-        d = self.route()
-
-        we_are_at = 'AAA'
+    def part1(self) -> None:
+        we_are_at = "AAA"
         steps = 0
-        while we_are_at != 'ZZZ':
+        for direction in self.route():
+            if we_are_at == "ZZZ":
+                break
             steps += 1
-            direction = next(d)
             we_are_at = self.maze[we_are_at][direction]
 
-        print(f'part1: {steps}')
+        print(f"part1: {steps}")
 
-    def part2(self):
-        starts = [ x for x in self.maze.keys() if x.endswith('A') ]
+    def part2(self) -> None:
+        starts = [x for x in self.maze.keys() if x.endswith("A")]
         ghoststeps = []
         for s in starts:
-            d = self.route()
             steps = 0
-            while s[2] != 'Z':
-                direction = next(d)
+            for direction in self.route():
+                if s[2] != "Z":
+                    break
                 steps += 1
                 s = self.maze[s][direction]
-            ghoststeps.append(steps)
-        print(f'part2: {math.lcm(*ghoststeps)}, number of ghosts: {len(starts)}')
 
+            # d = self.route()
+            # steps = 0
+            # while s[2] != 'Z':
+            # direction = next(d)
+            # steps += 1
+            # s = self.maze[s][direction]
+            ghoststeps.append(steps)
+        print(f"part2: {math.lcm(*ghoststeps)}, number of ghosts: {len(starts)}")
