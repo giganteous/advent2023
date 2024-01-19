4d56085f00634e54c47bcf2e5d501d9b8249e48c
Kai Storbeck <kai@xs4all.nl>
2024-01-19 23:48:45 +0100

    black

--- day18-lagoons/day18.py
+++ day18-lagoons/day18.py
@@ -4,10 +4,12 @@ import re
 from collections import namedtuple
 from typing import Tuple
 
-Point = namedtuple('Point', ['x', 'y'])
-Directive = namedtuple('Directive', ['dir', 'dist'])
+Point = namedtuple("Point", ["x", "y"])
+Directive = namedtuple("Directive", ["dir", "dist"])
 
-m = re.compile('^(?P<dir>[RLDU]) (?P<dist>[0-9]+) \((?P<rgb>[0-9a-f\#]{7})\)$')
+m = re.compile("^(?P<dir>[RLDU]) (?P<dist>[0-9]+) \((?P<rgb>[0-9a-f\#]{7})\)$")
+
+
 class Day18(BaseDay):
     colors: list[str] = []
     program: list[Directive] = []
@@ -18,24 +20,23 @@ class Day18(BaseDay):
                 r = m.match(line)
                 if r:
                     x = r.groupdict()
-                    self.program.append(Directive(x['dir'], int(x['dist'])))
-                    self.colors.append(x['rgb'])
+                    self.program.append(Directive(x["dir"], int(x["dist"])))
+                    self.colors.append(x["rgb"])
 
     @staticmethod
     def move(coord: Point, direction: int, dist: int) -> Point:
         # 0 -> x+=N; 1 -> y+=N; 2 -> x-=N; 3 -> y-=N;
         match (coord, direction, dist):
-            case ((x, y), 0 | 'R', l):
-                return Point(x+l, y)
-            case ((x, y), 1 | 'D', l):
-                return Point(x, y+l)
-            case ((x, y), 2 | 'L', l):
-                return Point(x-l, y)
-            case ((x, y), 3 | 'U', l):
-                return Point(x, y-l)
+            case ((x, y), 0 | "R", l):
+                return Point(x + l, y)
+            case ((x, y), 1 | "D", l):
+                return Point(x, y + l)
+            case ((x, y), 2 | "L", l):
+                return Point(x - l, y)
+            case ((x, y), 3 | "U", l):
+                return Point(x, y - l)
         print(coord, direction, dist)
         raise ValueError()
-
 
     def part1(self) -> None:
         coords = []
@@ -44,20 +45,22 @@ class Day18(BaseDay):
             dest = self.move(start, p.dir, p.dist)
             coords.append(dest)
             start = dest
-            if self.example: print(f'{p} -> {dest}')
+            if self.example:
+                print(f"{p} -> {dest}")
 
         mn = Point(min(p.x for p in coords), min(p.y for p in coords))
         mx = Point(max(p.x for p in coords), max(p.y for p in coords))
 
         def offset(p: Point) -> Point:
-            return Point(p.x-mn.x, p.y+mn.y)
-        size = Point(mx.x-mn.x+1, mx.y-mn.y+1)
+            return Point(p.x - mn.x, p.y + mn.y)
+
+        size = Point(mx.x - mn.x + 1, mx.y - mn.y + 1)
 
         if self.example:
-            print(f'min/max: {mn}, {mx}')
-            print(f'image dimensions: {mn}, {mx}')
-            print('offsets: ', -mn.x, -mn.y)
-            print('size: ', size)
+            print(f"min/max: {mn}, {mx}")
+            print(f"image dimensions: {mn}, {mx}")
+            print("offsets: ", -mn.x, -mn.y)
+            print("size: ", size)
 
         # create image
         image = Image.new("RGB", size, (255, 255, 255))
@@ -65,31 +68,31 @@ class Day18(BaseDay):
 
         start = Point(-mn.x, -mn.y)
         for end in (offset(a) for a in coords):
-            draw.line(start + end, fill=(0,255,0))
+            draw.line(start + end, fill=(0, 255, 0))
             start = end
-        middle = (size.x/2, size.y/2)
-        ImageDraw.floodfill(image, middle, (255,0,0))
+        middle = (size.x / 2, size.y / 2)
+        ImageDraw.floodfill(image, middle, (255, 0, 0))
         info = image.getcolors()
         if self.example:
             print(info)
-        image.save('lagoon.png')
-        print(sum(count for count, color in info if color != (255,255,255)))
-
+        image.save("lagoon.png")
+        print(sum(count for count, color in info if color != (255, 255, 255)))
 
     def part2(self) -> None:
         def shoelace(alist: list[Point], total_distance: int) -> int:
-            s1 = sum(a.x*b.y for a, b in zip(alist, alist[1:]))
-            s2 = sum(a.x*b.y for a, b in zip(alist[1:], alist))
+            s1 = sum(a.x * b.y for a, b in zip(alist, alist[1:]))
+            s2 = sum(a.x * b.y for a, b in zip(alist[1:], alist))
 
             # points within the boundary; Picks Theorem
-            interior = (s1 - s2) / 2 - (total_distance/2) + 1
+            interior = (s1 - s2) / 2 - (total_distance / 2) + 1
 
             # add the points under the boundary itself
             return int(interior) + total_distance
 
         def fixup(rgb: str) -> Directive:
             return Directive(int(rgb[6]), int(rgb[1:6], base=16))
-        #, int(rgb[6])
+
+        # , int(rgb[6])
 
         start = Point(0, 0)
         coords: list[Point] = [start]
